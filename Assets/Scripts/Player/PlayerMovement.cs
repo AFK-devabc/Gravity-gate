ca5f835af0f81eafc5279aa34eb2b25640ce9a25
AFK-devabc <20522152@gm.uit.edu.vn>
2022-10-03 22:04:13 +0700

    Feat : add rotation handler

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -5,14 +6,19 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float Speed;
     [SerializeField] private float jumpPower;
     [SerializeField] private LayerMask groundLayer;
-
+    [SerializeField] private float x_axisGravity;
+    [SerializeField] private float y_axisGravity;
     private Rigidbody2D body;
 
     float HorizontalInput;
 
     private BoxCollider2D boxCollider;
+    public bool IsTeleportable { get;set; }
+    private float ZrotationAngle;
+    float sinRotation;
+    float cosRotation;
 
-
+    Vector2 gravity;
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
@@ -22,8 +28,6 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         HorizontalInput = Input.GetAxis("Horizontal");
-
-        //Flip player when moving left/right
         if (HorizontalInput > 0.01f)
         {
             transform.localScale = Vector3.one;
@@ -32,20 +36,43 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.localScale = new Vector3(-1, 1, 1);
         }
-        body.velocity = new Vector2(HorizontalInput*Speed, body.velocity.y);
+
+
+        ZrotationAngle = transform.rotation.eulerAngles.z * Mathf.Deg2Rad;
+        sinRotation = MathF.Sin(ZrotationAngle);
+        cosRotation = MathF.Cos(ZrotationAngle);
+
+        GravityControl();
+
+        body.velocity = new Vector2(HorizontalInput*Speed * cosRotation, HorizontalInput * Speed * sinRotation )        // Left, right move (cos, sin)
+            + new Vector2(-sinRotation * body.velocity.x, cosRotation * body.velocity.y)                                //Jumping move      (-sin, cos)
+            + gravity* Time.deltaTime ;                                                                                 //gravity           (sin, -cos)
+        //Debug.Log(cosRotation);
         if (Input.GetKey(KeyCode.Space) )
             Jump();
     }
 
     private void Jump()
     {
-        if(IsGrounded())
-        body.velocity = new Vector2(HorizontalInput * Speed, jumpPower);
+        if (IsGrounded())
+            body.velocity = new Vector2(HorizontalInput * Speed * cosRotation
+                - jumpPower * sinRotation
+                , (HorizontalInput * Speed + body.velocity.y) * sinRotation
+                + jumpPower * cosRotation) ;
     }
 
     private bool IsGrounded()
     {
-        RaycastHit2D raycastHit2D = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        RaycastHit2D raycastHit2D = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(sinRotation, -cosRotation), 0.1f, groundLayer);  
         return raycastHit2D.collider != null;
     }
+
+    private void GravityControl()
+    {
+       
+        //gravity effects when the player rotate z axis
+        // G = (sin, -cos)
+        gravity = new Vector2(x_axisGravity * sinRotation  ,- y_axisGravity * cosRotation );
+    }
+
 }
---
+++ Assets/Scripts/Portal/Portal.cs
@@ -0,0 +1,39 @@
+using UnityEditor.MemoryProfiler;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    [SerializeField] private Portal ConnectedPortal;
+    private bool IsTeleportable = true;
+
+    [SerializeField] private Transform cameraTransform;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+     
+        if(collision.tag == "Player"  )   
+        {
+            if (IsTeleportable == true)
+            {
+                ConnectedPortal.SetTeleportable();
+
+
+                collision.GetComponent<Transform>().position = ConnectedPortal.transform.position;
+                cameraTransform.rotation = ConnectedPortal.transform.rotation;
+                collision.GetComponent<Transform>().rotation = ConnectedPortal.transform.rotation;
+                collision.GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
+
+            }
+        }
+    }
+
+    public void SetTeleportable()
+    {
+        IsTeleportable = false;
+
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+            IsTeleportable = true;
+    }
+}
